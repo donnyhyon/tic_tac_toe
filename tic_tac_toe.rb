0380c40be4803c7e3a5702075b3084bbc29b45c7
Donny <donny.hyon@madetech.com>
2022-06-23 14:44:25 +0100

    trying to get gets to work

---
+++ tic_tac_toe.rb
@@ -0,0 +1,29 @@
+=begin
+    
+PVP 
+[1] - create empty 3 x 3 grid
+
+[2] - prompt from terminal to ask for player 1 / 2 inputs. 
+
+[3] - input into grid
+input x ==> into grid
+
+how to define location?
+so what would input look like? 
+- start with co-ordinates. e.g (0,1) = first row, second column. (to middle)
+- 'bottom left, bottom right' - stretch goal
+
+return new grid with previous input.
+
+ask next player for their move. 
+
+create a win/ loose algorithm 
+-3 in a row wins. 
+- how does computer see 3 in a row? 
+- if all 9 squares are full, no player has 3 in a row, game is over. 
+
+future target: PVComputer
+
+end
+
+
