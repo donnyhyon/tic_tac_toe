955d99db412c2c87a87c0932718d7728a63df3c4
Donny <donny.hyon@madetech.com>
2022-07-05 10:20:38 +0100

    moved code to own file

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,37 +1,4 @@
-require 'stringio'
-
-def creates_new_grid
-    $board =  "\n 1 | 2 | 3 \n 4 | 5 | 6 \n 7 | 8 | 9\n" 
-    print $board
-end
-
-def get_input
-    $stdin.gets.chomp
-end
-
-def player_move(grid , player_marker, position)
-    if grid.include? position then
-        print grid.gsub!(position, player_marker)
-    else
-        raise "it doesnt exist"
-    end
-end
-
-def x_count(grid)
-    grid.count('X')
-end
-
-def o_count(grid)
-    grid.count('O')
-end
-
-def who_goes_next(x_count, o_count)
-    if x_count > o_count then
-        return 'O'
-    else return 'X'
-    end
-  end
-
+require_relative '../tic_tac_toe'
 
 describe 'creating the game board' do
     it 'ACCEPTANCE TEST: creates a 3 x 3 grid with 1- 9 position indicators' do
